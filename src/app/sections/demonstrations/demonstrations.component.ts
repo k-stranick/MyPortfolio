4b8c4f850fd4493a9c6f82e65727a611cc2fe2cf
Kyle <kstranic@dtcc.edu>
2025-02-15 10:51:57 -0500

    this should allow video usage

--- src/app/sections/demonstrations/demonstrations.component.ts
+++ src/app/sections/demonstrations/demonstrations.component.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
+import { YouTubePlayer, YouTubePlayerModule } from '@angular/youtube-player';
+
 
 @Component({
   selector: 'app-demonstrations',
   standalone: true,
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, YouTubePlayerModule],
   templateUrl: './demonstrations.component.html',
   styleUrl: './demonstrations.component.css'
 })
@@ -12,20 +14,22 @@ export class DemonstrationsComponent {
 
   // List of demo videos
   videoList = [
-    { name: 'Skyrend', src: 'assets/videos/skyrendc.mp4' },
-    { name: 'Weather App', src: 'assets/videos/weatherc.mp4' },
-    { name: 'Data Form', src: 'assets/videos/dataFormc.mp4' },
+    { name: 'Skyrend', src: 'https://youtu.be/c2DYt4gWh3E+', type: 'youtube' },
+    { name: 'Weather App', src: 'https://youtu.be/6SQazBvp30E', type: 'youtube' },
+    { name: 'Data Form', src: 'https://youtu.be/oVOpmv6qgI0', type: 'youtube' },
   ];
 
   //Signal to track the selected video
-  selectedVideo = signal<string | null>(null);
+  selectedVideo = signal<{ src: string | null, type: string | null }>({ src: null, type: null });
+  // selectedVideo = signal<string | null>(null);
 
   onVideoChange(event: Event) {
     const target = event.target as HTMLSelectElement;
-    this.selectedVideo.set(target.value || null);
+    const selectedOption = this.videoList.find(video => video.src === target.value);
+    this.selectedVideo.set(selectedOption ? { src: selectedOption.src, type: selectedOption.type } : { src: null, type: null });
   }
 
   clearVideo(): void {
-    this.selectedVideo.set(null);
+    this.selectedVideo.set({ src: null, type: null });
   }
 }
