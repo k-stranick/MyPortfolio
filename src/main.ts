cb31840022443bf6f4b21cd1245f74333e914cd1
Kyle <kstranic@dtcc.edu>
2025-05-22 22:56:37 -0400

    trying to fix github

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideHttpClient(), provideRouter(routes)],
 });
